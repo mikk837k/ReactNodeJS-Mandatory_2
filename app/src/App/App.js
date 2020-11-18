674fd447d18d8ca34ddeed014157351d1e75cb7b
Mikkel Madsen <mikk837k@stud.kea.dk>
2020-11-18 10:14:22 +0100

    React Router has been installed. Subfolders for the app, components and pages has been created in src folder

---
+++ app/src/App/App.js
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import './App.css';
+
+
+
+function App() {
+  return (
+    <Router>
+    
+    </Router>
+  );
+}
+
+export default App;
