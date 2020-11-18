f5776bd62289c06b95c34c889cebb56bb9053b73
Mikkel Madsen <mikk837k@stud.kea.dk>
2020-11-18 10:30:08 +0100

    Basic route skeleton build

--- app/src/App/App.js
+++ app/src/App/App.js
@@ -7,7 +7,36 @@ import './App.css';
 function App() {
   return (
     <Router>
+    <header>
+      <nav>
+        <div>
+          <Link to="/secret">Secret Page</Link>
+        </div>
+        {/* Knap components */}
+      </nav>
+    </header>
+    <main>
+      <Switch>
+        <Route exact path="/">
     
+        </Route>
+        <Route path="/signup">
+    
+        </Route>
+        <Route path="/login">
+    
+        </Route>
+        <Route path="/logout">
+    
+        </Route>
+        <Route path="/profile">
+    
+        </Route>
+        <Route path="/secret">
+    
+        </Route>
+      </Switch>
+    </main>
     </Router>
   );
 }
