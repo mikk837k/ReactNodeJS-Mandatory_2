2d7b6bad4f3fceb0c734616280d0acd6fb468d10
Daniel <daniel-beck@hotmail.com>
2020-11-28 12:28:52 +0100

    beginning test of login connection to backend

--- app/src/Components/forms/form_login.jsx
+++ app/src/Components/forms/form_login.jsx
@@ -1,12 +1,63 @@
 function Form_login() {
+
+    function checkEmail(e) {
+        const sEmailValue = e.target.value;
+
+        const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+        //Check if user input matches regex expression for email address
+        if (regex.test(sEmailValue)) {
+            e.target.style.border = "2px solid lightgreen";
+            return;
+        } else {
+            e.target.style.border = "2px solid red";
+            return;
+        }
+    }
+
+    async function submitForm(e) {
+        //Collect user data from form
+        let aUserData = e.target.parentElement.querySelectorAll(["[data-user]"]);
+        let oUserData = [];
+
+        //Iterate over array of user data
+        aUserData.forEach(element => {
+            //Get element name
+            let sElementName = element["name"];
+            //Get element value
+            let sElementValue = element.value;
+            //cCreate Object key value pairs based on element name(key) and value
+            oUserData[sElementName] = sElementValue;
+        })
+
+        fetch('http://localhost:8080/auth/login', {
+            method: 'POST',
+            body: oUserData,
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+        })
+            .then(res => res.json())
+            .then(res => console.log(res))
+
+
+        //Reset the form
+        // e.target.parentElement.reset();
+        // //Redirect user to index page
+        // document.location.href = "./";
+
+    }
+
+
     return (
         <form>
             <h1>Login</h1>
             <label>Email:</label>
-            <input type="text" placeholder="Type in your email here"></input>
+            <input name="email" type="text" data-user onInput={checkEmail} placeholder="Type in your email here" required></input>
             <label>Password:</label>
-            <input type="text" placeholder="Type in your password here"></input>
-            <button>Login</button>
+            <input name="password" type="password" data-user placeholder="Type in your password here" required></input>
+            <button type="button" onClick={submitForm}>Login</button>
         </form>
     )
 }
--- app/src/Components/forms/form_signup.jsx
+++ app/src/Components/forms/form_signup.jsx
@@ -6,101 +6,101 @@ function Form_signup() {
 
     function checkEmail(e) {
         const sEmailValue = e.target.value;
-        let eValdationField = document.querySelector("[name=validation]")
-    
+        let eValidationField = document.querySelector("[name=validation]")
+
         const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        
+
         //Check if user input matches regex expression for email address
-        if(regex.test(sEmailValue)){
+        if (regex.test(sEmailValue)) {
             e.target.style.border = "2px solid lightgreen";
-            eValdationField.setAttribute("data-isset-email", "1")
+            eValidationField.setAttribute("data-isset-email", "1")
             return;
         } else {
-            eValdationField.setAttribute("data-isset-email", "0")
+            eValidationField.setAttribute("data-isset-email", "0")
             e.target.style.border = "2px solid red";
             return;
         }
     }
-    
+
     function checkPassword() {
         const aPwElements = document.querySelectorAll("[data-password]");
-        let eValdationField = document.querySelector("[name=validation]");
-     
+        let eValidationField = document.querySelector("[name=validation]");
+
         //Check if password and confirm password are identical
-        if(aPwElements[0].value !== aPwElements[1].value){
-            eValdationField.setAttribute("data-isset-password", "0")
+        if (aPwElements[0].value !== aPwElements[1].value) {
+            eValidationField.setAttribute("data-isset-password", "0")
             aPwElements[1].style.border = "2px solid red";
             return;
-        }else {
-            eValdationField.setAttribute("data-isset-password", "1")
+        } else {
+            eValidationField.setAttribute("data-isset-password", "1")
             aPwElements[1].style.border = "2px solid lightgreen"
             return;
         }
     }
-    
+
     function checkUserName(e) {
-        
+
         //Check if the input field is empty or if the input value is less than the minimum allowed length
-        if(e.target.value === "" || e.target.value.length < parseInt(e.target.dataset.min)){
+        if (e.target.value === "" || e.target.value.length < parseInt(e.target.dataset.min)) {
             e.target.style.border = "2px solid red";
-        }else {
+        } else {
             e.target.style.border = "2px solid lightgreen";
         }
     }
-    
+
     function checkInputFields(aUserData) {
-        
+
         let iFieldApproved = 0;
-        
+
         //Iterate over array of elements containing user data.
         aUserData.forEach(element => {
             //Check if the input field is empty or if the input value is less than the minimum allowed length
-            if(element.value === "" || element.value.length < parseInt(element.dataset.min)){
+            if (element.value === "" || element.value.length < parseInt(element.dataset.min)) {
                 element.style.border = "2px solid red";
-            }else {
+            } else {
                 element.style.border = "2px solid lightgreen";
                 //Add to counter each time an element is ok
                 iFieldApproved++;
             }
         })
-        
+
         //Check if amount of approved fields are equal to the length of the array
-        if(iFieldApproved === aUserData.length) {
+        if (iFieldApproved === aUserData.length) {
             return true;
         } else {
             return false;
         }
     }
-    
+
     function checkForm(e) {
         const aUserData = e.target.parentElement.querySelectorAll(["[data-user]"]);
-        const eValdationField = document.querySelector("[name=validation]")
-        
+        const eValidationField = document.querySelector("[name=validation]")
+
         //Check if password and email fields are filled out
-        if(parseInt(eValdationField.getAttribute("data-isset-password")) && parseInt(eValdationField.getAttribute("data-isset-email"))) {
+        if (parseInt(eValidationField.getAttribute("data-isset-password")) && parseInt(eValidationField.getAttribute("data-isset-email"))) {
         } else {
-            if(!parseInt(eValdationField.getAttribute("data-isset-password"))) {
+            if (!parseInt(eValidationField.getAttribute("data-isset-password"))) {
                 return;
-            } else if(!parseInt(eValdationField.getAttribute("data-isset-email"))){
+            } else if (!parseInt(eValidationField.getAttribute("data-isset-email"))) {
                 return;
             }
         }
-        
+
         let iFormApproved = checkInputFields(aUserData);
-    
-       //Check the return value in iFormApproved
-        if(iFormApproved) {
+
+        //Check the return value in iFormApproved
+        if (iFormApproved) {
             submitForm(e, aUserData);
         }
-        
+
     }
-    
+
     async function submitForm(e, aUserData) {
         //Collect user data from form
         let oUserData = {};
-        
+
         //Iterate over array of user data
-        aUserData.forEach( element => {
+        aUserData.forEach(element => {
             //Get element name
             let sElementName = element["name"];
             //Get element value
@@ -114,28 +114,28 @@ function Form_signup() {
             method: "POST",
             body: oUserData
         })
-        .then(res => res.json())
-        .then(res => console.log(res))
+            .then(res => res.json())
+            .then(res => console.log(res))
 
         //Reset the form
         e.target.parentElement.reset();
         //Redirect user to index page
         // document.location.href = "./";
-    
+
     }
     return (
         <form>
             <h1>Signup</h1>
             <label>Firstname (min. 2 characters):</label>
-            <input name="firstName" data-user onInput={checkUserName}type="text" data-min="2" placeholder="Type in your firstname here" required></input>
+            <input name="firstName" data-user onInput={checkUserName} type="text" data-min="2" placeholder="Type in your firstname here" required></input>
             <label>Lastname (min. 2 characters):</label>
-            <input name="lastName" data-user onInput={checkUserName} type="text" data-min="2" placeholder="Type in your lastname here"required></input>
+            <input name="lastName" data-user onInput={checkUserName} type="text" data-min="2" placeholder="Type in your lastname here" required></input>
             <label>Email:</label>
-            <input name="email" data-user onInput={checkEmail} type="email" data-min="2" placeholder="Type in your email here"required></input>
+            <input name="email" data-user onInput={checkEmail} type="email" data-min="2" placeholder="Type in your email here" required></input>
             <label>Password (min. 2 characters):</label>
-            <input name="password" data-password onChange={checkPassword} data-user type="password" data-min="2" placeholder="Type in your password here"required></input>
+            <input name="password" data-password onChange={checkPassword} data-user type="password" data-min="2" placeholder="Type in your password here" required></input>
             <label>Confirm password:</label>
-            <input data-password type="password" onChange={checkPassword} placeholder="Type in your password here"required></input>
+            <input data-password type="password" onChange={checkPassword} placeholder="Type in your password here" required></input>
             <input name="validation" data-isset-email="0" data-isset-password="0" type="hidden"></input>
             <button type="button" onClick={checkForm}>Signup</button>
         </form>
